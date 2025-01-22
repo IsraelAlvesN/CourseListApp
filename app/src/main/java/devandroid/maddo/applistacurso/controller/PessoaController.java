99e0dd4e03a253d0ea5574574fc597f47f3a496b
IsraelAlvesN <devhardertresd@gmail.com>
2025-01-22 20:18:33 -0300

    Refactored the person controller class

--- app/src/main/java/devandroid/maddo/applistacurso/controller/PessoaController.java
+++ app/src/main/java/devandroid/maddo/applistacurso/controller/PessoaController.java
@@ -1,12 +1,23 @@
 package devandroid.maddo.applistacurso.controller;
 
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
 import devandroid.maddo.applistacurso.model.Pessoa;
+import devandroid.maddo.applistacurso.view.MainActivity;
 
 public class PessoaController {
+    SharedPreferences preferences;
+    SharedPreferences.Editor listaVip;
+    public static final String NOME_PREFERENCES = "pref_lista_vip";
+
+    public PessoaController(MainActivity mainActivity) {
+        preferences = mainActivity.getSharedPreferences(NOME_PREFERENCES, 0);
+        listaVip = preferences.edit();
+    }
+
     @NonNull
     @Override
     public String toString() {
@@ -17,5 +28,25 @@ public class PessoaController {
 
     public void salvar(Pessoa pessoa) {
         Log.d("Mvc Controller", "Salvo: " + pessoa.toString());
+
+        listaVip.putString("primeiroNome", pessoa.getPrimeiroNome());
+        listaVip.putString("sobreNome", pessoa.getSobreNome());
+        listaVip.putString("nomeCurso", pessoa.getCursoDesejado());
+        listaVip.putString("telefoneContato", pessoa.getTelefoneContato());
+        listaVip.apply();
+    }
+
+    public Pessoa buscar(Pessoa pessoa){
+        pessoa.setPrimeiroNome(preferences.getString("primeiroNome", ""));
+        pessoa.setSobreNome(preferences.getString("sobreNome", ""));
+        pessoa.setCursoDesejado(preferences.getString("nomeCurso", ""));
+        pessoa.setTelefoneContato(preferences.getString("telefoneContato", ""));
+
+        return pessoa;
+    }
+
+    public void limpar(){
+        listaVip.clear();
+        listaVip.apply();
     }
 }
